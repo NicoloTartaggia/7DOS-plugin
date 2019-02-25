065ce3e4187ea91c61690c9f83f2f332fe245236
Michele <mikelerogeratro@yahoo.it>
2019-02-25 17:51:12 +0100

    [json] sample(...) test

--- ProofOfConcept/barzrov/JsonTestScript.ts
+++ ProofOfConcept/barzrov/JsonTestScript.ts
@@ -144,7 +144,7 @@ class Network {
     }
 
     public sample(samples: number) {
-        this.network.sample(samples);
+        return this.network.sample(samples);
     }
 
     private buildNetworkFromJson(json_filename: string) {
@@ -184,7 +184,16 @@ class Network {
 
 console.log("---");
 console.log("Starting script");
+//First object build
 let Network1 = new Network("./bigNetwork.json");
+//Observe test
+Network1.observe("n3", new SingleValue("True", "1"));
+let samples = 1000;
+let sample_promise = Network1.sample(samples);
+sample_promise.then(function (result) {
+    console.log("Sample result:" + result / samples);
+});
+//Build another object
 let Network2 = new Network("./ourJson.json");
 console.log("Objects build done without errors");
 console.log("---");
