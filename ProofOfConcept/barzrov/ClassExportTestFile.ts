66fa63d521b404bbc5b2c2b64f2bbdf8b3929db3
Michele <mikelerogeratro@yahoo.it>
2019-02-28 12:26:53 +0100

    [json] created save function and fixed read function

--- ProofOfConcept/barzrov/ClassExportTestFile.ts
+++ ProofOfConcept/barzrov/ClassExportTestFile.ts
@@ -1,17 +1,32 @@
-import {Network, SingleValue} from "./JsonTestScript";
+import {Network, SingleValue} from "./JsonManager";
+import * as fs from "fs";
 
 console.log("---");
 console.log("Starting script");
-//First object build
-let Network1 = new Network("./bigNetwork.json");
-//Observe test
+
+//------------------
+//Costruisco due oggetti leggendo e passando al costruttore il contenuto del file json
+//Volendo è ovviamente possibile dargli direttamente la stringa del json nel costruttore
+//------------------
+
+//Costruisco la prima rete dal json bigNetwork.json
+let Network1 = new Network(fs.readFileSync("./bigNetwork.json", 'utf8'));
+//Faccio un test di observe e di sample
 Network1.observe("n3", new SingleValue("True", "1"));
 let samples = 1000;
 let sample_promise = Network1.sample(samples);
 sample_promise.then(function (result) {
     console.log("Sample result:" + result / samples);
 });
-//Build another object
-let Network2 = new Network("./ourJson.json");
+//Costruisco la seconda rete dal json ourJson.json
+let Network2 = new Network(fs.readFileSync("./ourJson.json", 'utf8'));
+//Scrivo la stringa json generata in un file per visualizzarla meglio
+//console.log("Network1.json:" + Network1.getNetworkJson());
+//console.log("Network2.json:" + Network2.getNetworkJson());
+fs.writeFileSync("OutputNetwork1.json", Network1.getNetworkJson());
+fs.writeFileSync("OutputNetwork2.json", Network2.getNetworkJson());
 console.log("Objects build done without errors");
 console.log("---");
+
+//Esempio di errore passando una stringa che non è un JSON
+//let NetworkBroken = new Network("fake json content");
