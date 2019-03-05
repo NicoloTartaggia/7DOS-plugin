952a2279aeca3d4940f49a42e7818c2bc998ff13
Michele <mikelerogeratro@yahoo.it>
2019-03-05 19:55:31 +0100

    Empty repository folder

--- src/components/config.ts
+++
@@ -1,9 +0,0 @@
-
-export class JsBayesAppConfigCtrl {
-  static templateUrl = 'components/config.html';
-  enabled: boolean;
-  appEditCtrl: any;
-  appModel: any;
-
-  constructor() {}
-}
--- src/panels/properties.ts
+++
@@ -1,8 +0,0 @@
-
-var pluginName = 'js-bayes',
-	diagramEditor = 'public/plugins/'+ pluginName +'/panels/partials/diagramEditor.html'
-
-export {
-	pluginName,
-	diagramEditor
-}
