908acd8a5b7f8c13f60719a7b427e562aa584d03
Michele <mikelerogeratro@yahoo.it>
2019-03-27 18:59:46 +0100

    Small tweaks to code

--- src/core/Network/Factory/ConcreteNetworkFactory.ts
+++ src/core/Network/Factory/ConcreteNetworkFactory.ts
@@ -5,46 +5,50 @@ import {AbstractValue} from "core/node/Value/AbstractValue";
 import {BoolValue} from "core/node/Value/BoolValue";
 import {RangeValue} from "core/node/Value/RangeValue";
 import {StringValue} from "core/node/Value/StringValue";
-import jsbayes from "jsbayes";
 import NetworkFactory from "./NetworkFactory";
+
+import jsbayes from "jsbayes";
 
 export default class ConcreteNetworkFactory implements NetworkFactory {
   // --------------------------------
   // NETWORK CREATION STUFF
 
-  private static addNode(node_name: string, network: JGraph, values_names: Array<string>): JNode {
+  private static addNode (node_name: string, network: JGraph, values_names: Array<string>): JNode {
     return network.addNode(node_name, values_names);
   }
 
-  private static setNodeParent(node: JNode, parent: JNode): void {
+  private static setNodeParent (node: JNode, parent: JNode): void {
     node.addParent(parent);
   }
 
-  private static setNodeCpt(node: JNode, cpt: Array<Array<number>>): void {
+  private static setNodeCpt (node: JNode, cpt: Array<Array<number>>): void {
     node.setCpt(cpt);
   }
 
   // --------------------------------
   // NETWORK CONTROL STUFF
 
-  private static nodeExist(node_name: string, node_dictionary: { [node_name: string]: JNode; }): boolean {
+  private static nodeExist (node_name: string, node_dictionary: { [node_name: string]: JNode; }): boolean {
     return Object.keys(node_dictionary).indexOf(node_name) >= 0;
   }
 
   // --------------------------------
   // JSON PARSING STUFF
 
-  private static parse_current_value(current_value): AbstractValue {
+  private static parse_current_value (current_value): AbstractValue {
     if (current_value.type === "boolean") {
       return new BoolValue(current_value.value, current_value.name);
     } else if (current_value.type === "range") {
       return new RangeValue(current_value.rangeMin, current_value.rangeMax, current_value.name);
     } else if (current_value.type === "string") {
       return new StringValue(current_value.value, current_value.name);
+    } else {
+      throw new Error("Found a JSON value that's not in the list boolean|range|string," +
+        "validate your JSON using the schema!");
     }
   }
 
-  public parseNetwork(file_content: string): ConcreateNetworkAdapter {
+  public parseNetwork (file_content: string): ConcreateNetworkAdapter {
     const network: JGraph = jsbayes.newGraph();
     const nodeList: Array<NodeAdapter> = new Array<NodeAdapter>();
     const node_dictionary: { [node_name: string]: JNode; } = {};
@@ -68,13 +72,16 @@ export default class ConcreteNetworkFactory implements NetworkFactory {
     // Begin to parse every node in the JSON file
     for (const node of (json_file).nodes) {
       const name = node.name;
+      // Create structures for current node
       node_parents_dictionary[name] = new Array<string>();
       node_values_dictionary[name] = new Array<AbstractValue>();
 
       // For each node i read his list of possible values
       const node_values_names: Array<string> = new Array<string>();
       for (const current_value of node.values) {
+        // Insert the value name in the JNode values
         node_values_names.push(current_value.name);
+        // Create the correct AbstractValue and save it in the list
         node_values_dictionary[name].push(ConcreteNetworkFactory.parse_current_value(current_value));
       }
 
@@ -87,7 +94,9 @@ export default class ConcreteNetworkFactory implements NetworkFactory {
       const name = node.name;
       for (const parent of node.parents) {
         if (ConcreteNetworkFactory.nodeExist(parent, node_dictionary)) {
+          // Set parent for JNode
           ConcreteNetworkFactory.setNodeParent(node_dictionary[name], node_dictionary[parent]);
+          // Insert parent in the list of current node parents
           node_parents_dictionary[name].push(parent);
         } else {
           throw new Error("Node " + parent + " not found in the network!");
@@ -98,6 +107,7 @@ export default class ConcreteNetworkFactory implements NetworkFactory {
     // For each node i read his CPT table
     for (const node of (json_file).nodes) {
       const name = node.name;
+      // TODO CHECK CORRECT SIZE USING node_parents_dictionary[name]
       ConcreteNetworkFactory.setNodeCpt(node_dictionary[name], node.cpt);
     }
     // --------------------------------
