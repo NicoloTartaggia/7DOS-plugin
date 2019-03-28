2c3f6360ee8b9196c806ba9634ea15023f93cf5c
ScrappyCocco <ScrappyCocco@users.noreply.github.com>
2019-03-28 12:13:21 +0100

    Added json and removed fake test

--- test/FactoryTest.ts
+++ test/FactoryTest.ts
@@ -2,11 +2,11 @@ import {ConcreteNetworkFactory} from "../src/core/network/Factory/ConcreteNetwor
 
 import {ConcreateNetworkAdapter} from "../src/core/network/Adapter/ConcreteNetworkAdapter";
 
-import { expect } from "chai";
+import {expect} from "chai";
 
 describe("ConcreteNetworkFactory", () => {
   it("Simple ConcreteNetworkFactory build test", () => {
-    const s: ConcreateNetworkAdapter = new ConcreteNetworkFactory().parseNetwork("{\"nodes\":[{\"name\":\"Example\",\"values\":[{\"name\":\"Example of string value\",\"type\":\"string\",\"value\":\"Value1?\"},{\"name\":\"Another example of string value\",\"type\":\"string\",\"value\":\"Value2?\"}],\"parents\":[\"Example2\"],\"cpt\":[[0.2,0.8],[0.8,0.2]]},{\"name\":\"Example2\",\"values\":[{\"name\":\"False\",\"type\":\"boolean\",\"value\":false},{\"name\":\"True\",\"type\":\"boolean\",\"value\":true}],\"parents\":[],\"cpt\":[[0.8,0.2]]},{\"name\":\"Example3\",\"values\":[{\"name\":\"Low Range\",\"type\":\"range\",\"rangeMin\":0,\"rangeMax\":10},{\"name\":\"Normal Range\",\"type\":\"range\",\"rangeMin\":11,\"rangeMax\":80},{\"name\":\"Alert Range\",\"type\":\"range\",\"rangeMin\":81,\"rangeMax\":100}],\"parents\":[],\"cpt\":[[0.81,0.2]]}]}");
+    const s: ConcreateNetworkAdapter = new ConcreteNetworkFactory().parseNetwork('{"nodes":[{"name":"Example","values":[{"name":"Example of string value","type":"string","value":"Value1?"},{"name":"Another example of string value","type":"string","value":"Value2?"}],"parents":["Example2"],"cpt":[[0.2,0.8],[0.8,0.2]]},{"name":"Example2","values":[{"name":"False","type":"boolean","value":false},{"name":"True","type":"boolean","value":true}],"parents":[],"cpt":[[0.8,0.2]]},{"name":"Example3","values":[{"name":"Low Range","type":"range","rangeMin":0,"rangeMax":10},{"name":"Normal Range","type":"range","rangeMin":11,"rangeMax":80},{"name":"Alert Range","type":"range","rangeMin":81,"rangeMax":100}],"parents":[],"cpt":[[0.81,0.2]]}]}');
     expect(s.getNodeList().length).to.equal(3);
   });
 });
--- test/HelloTest.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-
-describe("Stack", () => {
-  it("can be initialized without an initializer", () => {
-    const s = "Hello";
-    expect(s).to.equal("Hello");
-  });
-});
