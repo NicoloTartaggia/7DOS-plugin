f09564f436dde02a94937caebca662c1076eeb94
UdrK <nomefittizioqualunque@gmail.com>
2019-04-01 12:27:47 +0200

    test network

--- src/test/Network_Tests/ConcreteNetworkFactoryTest.ts
+++ src/test/Network_Tests/ConcreteNetworkFactoryTest.ts
@@ -6,16 +6,64 @@ import {expect} from "chai";
 
 describe("ConcreteNetworkFactory - parseNetwork", () => {
   it("Correct network", () => {
-    let str: string = '{"nodes":[{"name":"Example","values":[{"name":"Example of string value","type":"string","value":"Value1?"},{"name":"Another example of string value","type":"string","value":"Value2?"}],"parents":["Example2"],"cpt":[[0.2,0.8],[0.8,0.2]]},{"name":"Example2","values":[{"name":"False","type":"boolean","value":false},{"name":"True","type":"boolean","value":true}],"parents":[],"cpt":[[0.8,0.2]]},{"name":"Example3","values":[{"name":"Low Range","type":"range","rangeMin":0,"rangeMax":10},{"name":"Normal Range","type":"range","rangeMin":11,"rangeMax":80},{"name":"Alert Range","type":"range","rangeMin":81,"rangeMax":100}],"parents":[],"cpt":[[0.81,0.2]]}]}';
-    const s: ConcreteNetworkAdapter = new ConcreteNetworkFactory().parseNetwork(str);
-    expect(s.getNodeList().length).to.equal(3);
+    const json = require("./CorrectNetwork.json");
+    const jsonString: string = JSON.stringify(json);
+    const s: ConcreteNetworkAdapter = new ConcreteNetworkFactory().parseNetwork(jsonString);
+    expect(s.getNodeList().length).to.equal(2);
   });
   it("Null check", () => {
     let str: string;
     expect(() => new ConcreteNetworkFactory().parseNetwork(str)).to.throw(Error, "JSON file content is null...");
   });
-  it("Invalid JSON", () => {
+  it("Empty JSON", () => {
     let str: string = "";
-    expect(() => new ConcreteNetworkFactory().parseNetwork(str)).to.throw(Error, "");
+    //new ConcreteNetworkFactory().parseNetwork(str)
+    expect(() => new ConcreteNetworkFactory().parseNetwork(str)).to.throw(Error, "Bad Json Content!");
+  });
+  it("Invalid JSON", () => {
+    let json = require("./InvalidNetwork.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "");
+  });
+  it("Broken JSON", () => {
+    const jsonString = '{"nodes": {[)}}';
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "Bad Json Content!");
+  });
+  it("Incorrect cpt probabilities", () => {
+    let json = require("./IncorrectCpt.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error);
+  });
+  it("MinRange > MaxRange", () => {
+    let json = require("./InvertedMinMax.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(TypeError, "maxRange is less then minRange");
+  });
+  it("Incorrect node value type", () => {
+    let json = require("./IncorrectType.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(TypeError, "invalid parameter");
+  });
+  it("Non existing node parent", () => {
+    let json = require("./IncorrectParent.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "Node FakeParent not found in the network!");
+  });
+  it("Direct circular parenthood", () => {
+    let json = require("./DCircularParenthood.json");
+    const jsonString = JSON.stringify(json);
+    new ConcreteNetworkFactory().parseNetwork(jsonString);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "Circular parenthood");
+  });
+  it("Indirect circular parenthood", () => {
+    let json = require("./ICircularParenthood.json");
+    const jsonString = JSON.stringify(json);
+    new ConcreteNetworkFactory().parseNetwork(jsonString);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "Circular parenthood");
+  });
+  it("Two nodes with same name", () => {
+    let json = require("./IncorrectName.json");
+    const jsonString = JSON.stringify(json);
+    expect(() => new ConcreteNetworkFactory().parseNetwork(jsonString)).to.throw(Error, "The node Example2 already exist in the network!");
   });
 });
