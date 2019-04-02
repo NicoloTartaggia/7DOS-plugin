2f92fb79b7962cf01dad8ffc88d8e7aec7be236a
knowbot <andrea.trevisin@studenti.unipd.it>
2019-04-02 16:57:56 +0200

    deleted some junk files

--- src/core/database_client/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sync_1 = require("sync");
-function aSync() {
-    return 5;
-}
-console.log(sync_1["default"].sync(aSync()));
--- src/core/database_client/test.ts
+++
@@ -1,7 +0,0 @@
-import Sync from "sync";
-
-function aSync () {
-  return 5;
-}
-
-console.log(Sync.sync(aSync()));
