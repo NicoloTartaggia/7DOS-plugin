940e9d726d22e54f566ee4f921933cc10d0c9418
Michele <mikelerogeratro@yahoo.it>
2019-04-03 18:25:05 +0200

    build fix

--- src/core/net_manager/result/calculation_result/CalcResult.ts
+++ src/core/net_manager/result/calculation_result/CalcResult.ts
@@ -4,24 +4,26 @@ export class CalcResult {
   private readonly nodeName: string;
   private readonly items: Array<CalcResultItem>;
 
-  public constructor(nodeName: string, items: Array<CalcResultItem>) {
-    if(nodeName==null||items==null){
+  public constructor (nodeName: string, items: Array<CalcResultItem>) {
+    if (nodeName == null || items == null) {
       throw new Error("invalid parameter");
     }
-    for(let item of items){
-      if(item===null){
+    for (const item of items) {
+      if (item === null) {
         throw new Error("invalid parameter");
       }
     }
     this.nodeName = nodeName;
     this.items = items;
   }
-  public getNodeName(): string {
+
+  public getNodeName (): string {
     return this.nodeName;
   }
-  public getValueProbs(): Array<CalcResultItem> {
-    let copyProbs:Array<CalcResultItem>=new Array<CalcResultItem>();
-    for(let item of this.items){
+
+  public getValueProbs (): Array<CalcResultItem> {
+    const copyProbs: Array<CalcResultItem> = new Array<CalcResultItem>();
+    for (const item of this.items) {
       copyProbs.push(item);
     }
     return copyProbs;
--- src/core/net_manager/result/calculation_result/CalcResultItem.ts
+++ src/core/net_manager/result/calculation_result/CalcResultItem.ts
@@ -2,17 +2,19 @@ export class CalcResultItem {
   private readonly valueName: string;
   private readonly probValue: number;
 
-  public constructor(valueName: string, probValue: number) {
-    if(valueName===null||probValue===null || probValue<0 || probValue>1){
+  public constructor (valueName: string, probValue: number) {
+    if (valueName === null || probValue === null || probValue < 0 || probValue > 1) {
       throw new Error("invalid parameter");
     }
     this.valueName = valueName;
     this.probValue = probValue;
   }
-  public getValueName(): string {
+
+  public getValueName (): string {
     return this.valueName;
   }
-  public getProbValue(): number {
+
+  public getProbValue (): number {
     return this.probValue;
   }
 
