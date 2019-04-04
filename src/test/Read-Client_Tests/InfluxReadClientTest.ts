fa9ac32c28565254132fb9916abfa21d3e6503a2
Unknown <gtm459@gmail.com>
2019-04-04 20:05:50 +0200

    aggiunto before/after commentati perche' non funzionano ancora

--- src/test/Read-Client_Tests/InfluxReadClientTest.ts
+++ src/test/Read-Client_Tests/InfluxReadClientTest.ts
@@ -2,7 +2,7 @@
 // import { ConcreteReadClientFactory } from "../../core/read-client/ReadClientFactory";
 
 import {expect} from "chai";
-
+//import {InfluxDB} from "influx";
 describe("InfluxReadClient - createReaderClient", () => {
     it(" TODO ", () => {
         // TODO
@@ -16,19 +16,47 @@ describe("InfluxReadClient - getAddress", () => {
         expect(true).to.equal(true);
     });
 });
-
 describe("InfluxReadClient - readField", () => {
+/*
+  before(function(){
+    this.timeout(10000);
+    let client:InfluxDB=new InfluxDB({
+      host: 'localhost',
+      port: 8086,
+      database:"readTest"
+    });
+    return new Promise((resolve) => {
+      client.createDatabase("readTest").then(function(){
+        client.writePoints([{
+          measurement:"cpu",
+          tags:{},
+          fields:{value:0.64}
+        }]);
+      });
+      resolve();
+    }); 
+  });
   it("read on influx server- field read correctly", () => {
-    // TODO FIX USING PRE-TEST
-    /*let client:InfluxReadClient=new ConcreteReadClientFactory().makeInfluxReadClient("http://localhost","8086");
-    let result=client.readField("readTest", "select * from cpu");
-    result.then(function(value){
-      expect(value[0].rows[0].value).to.equal(0.64);
-    },
-    function(value){
-      throw new Error("Influx error");
+      return new Promise((resolve) => {
+        let client:InfluxReadClient=new ConcreteReadClientFactory().makeInfluxReadClient("http://localhost","8086");
+        let result=client.readField("readTest", "select value from cpu");
+        result.then(function(value){
+          expect(value[0].rows[0].value).to.equal(0.64);
+        });
+        resolve();
+      });
+  });
+  after(function(){
+    return new Promise((resolve) => {
+      let client:InfluxDB=new InfluxDB({
+        host: 'localhost',
+        port: 8086,
+      });
+      client.dropDatabase("readTest");
+      resolve();
     });
   });
+  */
   it("null database - exception thrown",()=>{
     let client:InfluxReadClient=new ConcreteReadClientFactory().makeInfluxReadClient("http://localhost","8086");
     client.readField(null,":8086").then(function(){})
